f383ac5e4a99614ab9a74c4a11234dd5c1cbd8b8
warlock <h.kwiatkowski@hotmail.com>
2021-11-30 12:37:43 +0100

    added high score

--- tetris.py
+++ tetris.py
@@ -262,34 +262,78 @@ def draw_next_shape(shape, surface):
                 pygame.draw.rect(surface, shape.color, (sx + j*30, sy + i*30, 30, 30), 0)
  
     surface.blit(label, (sx + 10, sy- 30))
+
+
+def update_score(nscore):
+    score = max_score()
+
+    with open('scores.txt', 'w') as f:
+        if int(score) > nscore:
+            f.write(str(score))
+        else:
+            f.write(str(nscore))
+
+
+def max_score():
+    with open('scores.txt', 'r') as f:
+        lines = f.readlines()
+        score = lines[0].strip()
+
+    return score
+
  
- 
-def draw_window(surface, score=0):
+def draw_window(surface, score=0, last_score=0):
     surface.fill((0,0,0))
     font = pygame.font.SysFont('comicsans', 60)
     label = font.render('TETRIS', 1, (255,255,255))
  
     surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 30))
  
+    # current score
     font = pygame.font.SysFont('comicsans', 30)
-    label = font.render('Score: ' + str(score), 1, (255,255,255))
+    label = font.render(
+        'Score: ' + str(score), 
+        1, 
+        (255,255,255))
  
     sx = top_left_x + play_width + 50
     sy = top_left_y + play_height/2 - 100
 
-    surface.blit(label, (sx + 30, sy + 150))
+    surface.blit(label, (sx + 30, sy + 160))
+
+    # last score
+    label = font.render(
+        'High Score: ' + last_score, 
+        1, 
+        (255,255,255))
+ 
+    sx = top_left_x - 250
+    sy = top_left_y + 200
+
+    surface.blit(label, (sx + 30, sy + 160))
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            pygame.draw.rect(surface, grid[i][j], (top_left_x + j* 30, top_left_y + i * 30, 30, 30), 0)
+            pygame.draw.rect(
+                surface, 
+                grid[i][j], 
+                (top_left_x + j* 30, top_left_y + i * 30, 30, 30), 
+                0
+            )
  
     draw_grid(surface, 20, 10)
-    pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 5)
+    pygame.draw.rect(
+        surface, 
+        (255, 0, 0), 
+        (top_left_x, top_left_y, play_width, play_height), 
+        5
+    )
  
  
 def main():
     global grid
- 
+
+    last_score = max_score()
     locked_positions = {}  # (x,y):(255,0,0)
     grid = create_grid(locked_positions)
  
@@ -364,7 +408,7 @@ def main():
  
             score += clear_rows(grid, locked_positions) * 10
  
-        draw_window(win, score)
+        draw_window(win, score, last_score)
         draw_next_shape(next_piece, win)
         pygame.display.update()
  
@@ -373,6 +417,7 @@ def main():
             pygame.display.update()
             pygame.time.delay(1500)
             run = False
+            update_score(score)
  
  
 def main_menu():
